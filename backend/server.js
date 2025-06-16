daaa6ed9e383bfe6e1ec3ee712d4533bd39d6d3c
Bashu <bawasthi089@gmail.com>
2025-06-16 14:38:12 +0545

    backend url

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ const app = express()
 const PORT = 5000;
 
 // Allowed multiple origins
-const allowedOrigins = ['http://localhost:5173','https://greencart-frontend-jade.vercel.app']
+const allowedOrigins = ['http://localhost:5173','https://greencart-frontend-jade.vercel.app','https://vercel.com/bashu-awasthis-projects/greencart-backend']
 
 // Middleware configuration
 app.use(express.json())
